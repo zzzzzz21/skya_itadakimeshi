905398b59751f0d6de57c3fb89cff35591432142
KuraharaMasahiro <kuraharamasahiro@gmail.com>
2023-05-21 18:30:49 +0900

    fixed

--- src/scripts/modules/disclosure.js
+++ src/scripts/modules/disclosure.js
@@ -8,6 +8,8 @@ export default class Disclosure {
     this.content = this.root.querySelector('.js-disclosure__content');
     this.controlledElment = false;
     const id = this.button.getAttribute('aria-controls');
+    this.openLabel = this.button.getAttribute('data-label-open');
+    this.closeLabel = this.button.getAttribute('data-label-close');
 
     if (id) {
       this.controlledElment = this.root.querySelector(`#${id}`);
@@ -39,9 +41,11 @@ export default class Disclosure {
   toggleExpand() {
     if (this.button.getAttribute('aria-expanded') === 'true') {
       this.button.setAttribute('aria-expanded', 'false');
+      this.button.innerHTML = `<span>${this.openLabel}</span>`;
       this.hideContent();
     } else {
       this.button.setAttribute('aria-expanded', 'true');
+      this.button.innerHTML = `<span>${this.closeLabel}</span>`;
       this.showContent();
     }
   }
